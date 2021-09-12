e70009d012563516009de7987c4bffef4fcb8f55
itsbot <patrickiannini@knights.ucf.edu>
2021-09-12 18:13:06 -0400

    Initial commit

---
+++ src/main/java/Exercise18/Exercise18.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 18 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package Exercise18;
+
+public class Exercise18 {
+    public static void main(String[] args) {
+        
+    }
+}
