656a5316bb2ff9712bcd39b3de443f020e125e44
itsbot <patrickiannini@knights.ucf.edu>
2021-09-12 19:12:01 -0400

    Completed Exercise

--- src/main/java/Exercise18/Exercise18.java
+++ src/main/java/Exercise18/Exercise18.java
@@ -3,9 +3,30 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise18;
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercise18 {
     public static void main(String[] args) {
-        
+        System.out.print("Press C to convert from Fahrenheit to Celsius\nPress F to convert from Celsius to Fahrenheit\nYour choice: ");
+        Scanner sc = new Scanner(System.in);
+
+        String from =  sc.next().toUpperCase(Locale.ROOT);
+        double temp;
+        DecimalFormat df = new DecimalFormat("0.##");
+
+        if (from.charAt(0) == 'C') {
+            System.out.println("Please enter the temperature in Fahrenheit");
+            temp = sc.nextDouble();
+            double C = (temp-32) *5/9;
+            System.out.println("The temperature in Celsius is "+df.format(C));
+        } else if (from.charAt(0) == 'F') {
+            System.out.println("Please enter the temperature in Celsius");
+            temp = sc.nextDouble();
+            double F = (temp*9/5) +32;
+            System.out.println("The temperature in Fahrenheit is "+df.format(F));
+        }
+        sc.close();
     }
 }
